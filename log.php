9ad4d75ebb2dcba79cb526c5b90d62e2bd5708c1
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-04 11:55:33 +0000

    Update log.php

--- log.php
+++ log.php
@@ -29,7 +29,7 @@ include "version.php";
   <title>SvxLink Dashboard for <?php echo getConfigItem(SVXLOGICSECTION, "CALLSIGN", $configs); ?></title>
 
   <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="include/bootstrap.css">
 
 </head>
 
