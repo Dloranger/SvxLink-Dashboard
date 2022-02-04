48f61e5c706e51719ada4976bbda27211a8674b4
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-04 11:59:59 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $time = explode(' ', $time);
 $time = $time[1] + $time[0];
 $start = $time;
 header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+header("Expires: Sat, 31 Dec 2022 05:00:00 GMT");
 // do not touch these includes!
 include "config/config.php";
 include "include/tools.php";
