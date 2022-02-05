7c84ca00885d2338ddd827c0a4a478c770647fe8
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-05 19:52:37 +0000

    Update tgstatus.php

--- tgstatus.php
+++ tgstatus.php
@@ -1,16 +1,15 @@
 <?php
 
-/*include "config/config.php";
+include "config/config.php";
 include "include/tools.php";
 include "include/functions.php";
-
 $configs = getSvxConfig();
-$txStatus = getSvxTGLines();
+//$txStatus = getSvxTGLines();
 
-if (strpos($tgStatus,"ON")) {
+//if (strpos($tgStatus,"ON")) {
 	echo "<tr><td class=\"table-danger text-center\">ON</td></tr>";
-} else {
-	echo "<tr><td class=\"table-success text-center\">OFF</td></tr>";
-}
-*/
+//} else {
+//	echo "<tr><td class=\"table-success text-center\">OFF</td></tr>";
+//}
+
 ?>
