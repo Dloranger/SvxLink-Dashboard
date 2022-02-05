9ac9a749aa05a6c76c699108be15fceaefb7fa1a
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-02-05 17:29:48 +0000

    Update tgstatus.php

--- include/tgstatus.php
+++ include/tgstatus.php
@@ -1,1 +1,54 @@
-
+  <div class="card">
+    <div class="card-header bg-primary text-light">
+      Transmitter Status
+    </div>
+    <div class="table-responsive">
+      <table id="currtx" class="table table-condensed table-striped table-hover">
+        <thead>
+          <tr>
+            <th>TG State</th>
+          </tr>
+        </thead>
+        <tbody id="tgline">
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <script>
+  function doXMLHTTPRequest(scriptname, elem) {
+	var xmlhttp;
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	} else {// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			document.getElementById(elem).innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET",scriptname,true);
+	xmlhttp.send();
+  }
+  function refreshInQSOAndLastHeardList() {
+	doXMLHTTPRequest("tgstatus.php","tgline");
+  }
+  var transmitting = false;
+  function loadXMLDoc() {
+	var xmlhttp;
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	} else {// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			document.getElementById("tgline").innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET","tgstatus.php",true);
+	xmlhttp.send();
+	var timeout = window.setTimeout("loadXMLDoc()", 1000);
+  }
+  loadXMLDoc();
+  </script>
